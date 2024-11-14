bdbde367270888b9d60a0ce37f7a4a23ae9ebafd
Jorge-Andres17 <jorgea.ibarrag@uqvirtual.edu.co>
2024-11-13 22:42:22 -0500

    creacion de persona y administrador

--- src/main/java/co/edu/uniquindio/poo/Administrador.java
+++ src/main/java/co/edu/uniquindio/poo/Administrador.java
@@ -1,5 +1,39 @@
 package co.edu.uniquindio.poo;
 
-public class Administrador {
+public class Administrador extends Persona {
+    private String usuario;
+    private String clave;
+    private String preguntaSeguridad;
+
+    public Administrador(String nombre, String cedula, long telefono, String usuario, String clave, String preguntaSeguridad){
+        super(nombre, cedula, telefono);
+        this.usuario = usuario;
+        this.clave = clave;
+        this.preguntaSeguridad = preguntaSeguridad;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getPreguntaSeguridad() {
+        return preguntaSeguridad;
+    }
+
+    public void setPreguntaSeguridad(String preguntaSeguridad) {
+        this.preguntaSeguridad = preguntaSeguridad;
+    }
 
 }
--- src/main/java/co/edu/uniquindio/poo/Persona.java
+++ src/main/java/co/edu/uniquindio/poo/Persona.java
@@ -1,5 +1,38 @@
 package co.edu.uniquindio.poo;
 
 public class Persona {
+    private String nombre;
+    private String cedula;
+    private long telefono;
+
+    public Persona(String nombre, String cedula, long telefono) {
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public long getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(long telefono) {
+        this.telefono = telefono;
+    }
 
 }
